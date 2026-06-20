4bfa0c092dc526647e2cb1408133dcbae80596ef
CPea <cpea2506@gmail.com>
2026-06-20 16:49:00 +0700

    refactor: prefer `default` attribute to `hlexists` when defining highlight

--- lua/select/init.lua
+++ lua/select/init.lua
@@ -2,12 +2,9 @@ local M = {}
 
 function M.setup(opts)
     local config = require "select.config"
-
-    if vim.fn.hlexists "SelectOptionLabel" == 0 then
-        vim.api.nvim_set_hl(0, "SelectOptionLabel", { link = "Type" })
-    end
-
     config.extend(opts)
+
+    vim.api.nvim_set_hl(0, "SelectOptionLabel", { link = "Type", default = true })
 
     vim.ui.select = require "select.select"
 end
